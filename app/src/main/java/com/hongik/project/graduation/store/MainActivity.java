532563d6b3c839ff3403477ec0d848a8c6eb6121
yongCloude <djkye0001@naver.com>
2022-06-14 19:05:18 +0900

    [feat] drawer 추가

--- app/src/main/java/com/hongik/project/graduation/store/MainActivity.java
+++ app/src/main/java/com/hongik/project/graduation/store/MainActivity.java
@@ -1,14 +1,38 @@
 package com.hongik.project.graduation.store;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private ImageButton menuButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        menuButton = findViewById(R.id.menu_bar);
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+
+
+        menuButton.setOnClickListener(view -> {
+
+            if(!drawerLayout.isDrawerOpen(Gravity.LEFT)){
+                drawerLayout.openDrawer(GravityCompat.START);
+            } else {
+                drawerLayout.closeDrawer(Gravity.LEFT);
+            }
+
+        });
     }
+
 }
