7114161f1fcf2e870931becf17834a136744b405
sumin-yang224 <kzx1547p@gmail.com>
2022-06-14 18:26:42 +0900

    [fix] 디렉토리 재설정

--- app/src/main/java/com/hongik/project/graduation/store/activity/LogoutActivity.java
+++ app/src/main/java/com/hongik/project/graduation/store/activity/LogoutActivity.java
@@ -1,8 +1,10 @@
-package com.hongik.project.graduation.store;
+package com.hongik.project.graduation.store.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.hongik.project.graduation.store.R;
 
 public class LogoutActivity extends AppCompatActivity {
 
