02859fedf4e71657e39724a224850683f3ea1955
sumin-yang224 <kzx1547p@gmail.com>
2022-06-16 13:11:52 +0900

    [create] 파일 생성

--- app/src/main/java/com/hongik/project/graduation/store/ui/activity/MainActivity.java
+++ app/src/main/java/com/hongik/project/graduation/store/ui/activity/MainActivity.java
@@ -4,17 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.widget.ImageButton;
 
 
 import com.hongik.project.graduation.store.R;
+import com.hongik.project.graduation.store.ui.fragment.CouponDealBoardFragment;
+import com.hongik.project.graduation.store.ui.fragment.EventListFragment;
 import com.hongik.project.graduation.store.ui.fragment.NaviContentFragment;
 import com.hongik.project.graduation.store.ui.fragment.NaviHeaderFragment;
+import com.hongik.project.graduation.store.ui.fragment.UserRecommendationFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = "MainActivity";
 
     private DrawerLayout drawerLayout;
     private ImageButton menuButton;
@@ -27,14 +34,10 @@ public class MainActivity extends AppCompatActivity {
         Fragment naviHeaderFragment = getSupportFragmentManager().findFragmentById(R.id.navigation_header_container);
         Fragment naviContentFragment = getSupportFragmentManager().findFragmentById(R.id.navigation_content_container);
 
-
         initMenuBar(naviHeaderFragment, naviContentFragment);
-
 
         menuButton = findViewById(R.id.menu_bar);
         drawerLayout = findViewById(R.id.drawer_layout);
-
-
 
         menuButton.setOnClickListener(view -> {
 
@@ -45,6 +48,70 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onResume() called");
+
+        Fragment userRecommendationFragment = getSupportFragmentManager().findFragmentById(R.id.user_recommendation_fragment_container);
+        Fragment eventListFragment = getSupportFragmentManager().findFragmentById(R.id.event_list_fragment_container);
+        Fragment couponDealBoardFragment = getSupportFragmentManager().findFragmentById(R.id.coupon_deal_board_fragment_container);
+
+        initMainContent(userRecommendationFragment, eventListFragment, couponDealBoardFragment);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+
+    }
+
+    private void initMainContent(Fragment userRecommendationFragment, Fragment eventListFragment, Fragment couponDealBoardFragment) {
+        if(userRecommendationFragment == null){
+            UserRecommendationFragment recommendationFragment = new UserRecommendationFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.user_recommendation_fragment_container, recommendationFragment)
+                    .commit();
+        }
+
+        if(eventListFragment == null){
+            EventListFragment eventFragment = new EventListFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.event_list_fragment_container, eventFragment)
+                    .commit();
+        }
+
+        if(couponDealBoardFragment == null){
+            CouponDealBoardFragment couponFragment = new CouponDealBoardFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.coupon_deal_board_fragment_container, couponFragment)
+                    .commit();
+        }
     }
 
     private void initMenuBar(Fragment naviHeaderFragment, Fragment naviContentFragment) {
